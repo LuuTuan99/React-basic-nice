2143e05fa190dbc8ff5c38ec08dd2535acc3aa07
Tuans <luucungtuan2907@gmail.com>
2020-11-04 00:23:45 +0700

    show infor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
 import React, { Component } from "react";
 import TaskForm from "./component/TaskForm";
 import Control from "./component/Control";
+import TaskList from "./component/TaskList";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [], // id: unique, name, status
+    };
+  }
+
+  componentWillMount() {
+    if (localStorage && localStorage.getItem("tasks")) {
+      var tasks = JSON.parse(localStorage.getItem("tasks"));
+      this.setState({
+        tasks: tasks,
+      });
+    }
+  }
+
+  onGenerateData = () => {
+    var tasks = [
+      {
+        id: this.generateID(),
+        name: "Hoc Java",
+        status: true,
+      },
+      {
+        id: this.generateID(),
+        name: "Hoc React",
+        status: false,
+      },
+      {
+        id: this.generateID(),
+        name: "Hoc Angular",
+        status: true,
+      },
+    ];
+    this.setState({
+      tasks: tasks,
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  generateID() {
+    return (
+      this.s4() +
+      this.s4() +
+      "-" +
+      this.s4() +
+      this.s4() +
+      "-" +
+      this.s4() +
+      this.s4() +
+      "-" +
+      this.s4() +
+      this.s4() +
+      "-" +
+      this.s4() +
+      this.s4()
+    );
+  }
+
   render() {
+    var { tasks } = this.state; // var tasks = this.sate.tasks;
+
     return (
       <div className="container">
         <div className="text-center">
@@ -19,55 +88,20 @@ class App extends Component {
               <span className="fa fa-plus mr-5" />
               Thêm Công Việc
             </button>
+            &nbsp;
+            <button
+              type="button"
+              className="btn btn-danger ml-5"
+              onClick={this.onGenerateData}
+            >
+              Genrate Data
+            </button>
             {/* Search - Sort */}
             <Control />
             {/* List */}
             <div className="row mt-15">
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <table className="table table-bordered table-hover">
-                  <thead>
-                    <tr>
-                      <th className="text-center">STT</th>
-                      <th className="text-center">Tên</th>
-                      <th className="text-center">Trạng Thái</th>
-                      <th className="text-center">Hành Động</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td />
-                      <td>
-                        <input type="text" className="form-control" />
-                      </td>
-                      <td>
-                        <select className="form-control">
-                          <option value={-1}>Tất Cả</option>
-                          <option value={0}>Ẩn</option>
-                          <option value={1}>Kích Hoạt</option>
-                        </select>
-                      </td>
-                      <td />
-                    </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>Học lập trình</td>
-                      <td className="text-center">
-                        <span className="label label-success">Kích Hoạt</span>
-                      </td>
-                      <td className="text-center">
-                        <button type="button" className="btn btn-warning">
-                          <span className="fa fa-pencil mr-5" />
-                          Sửa
-                        </button>
-                        &nbsp;
-                        <button type="button" className="btn btn-danger">
-                          <span className="fa fa-trash mr-5" />
-                          Xóa
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+                <TaskList tasks={tasks} />
               </div>
             </div>
           </div>
